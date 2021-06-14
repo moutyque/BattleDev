468ecc7fdbd57cc8d1f2e94ccfb8da62c053f0c0
Quentin M <azfboom@gmail.com>
2021-06-14 22:13:22 +0200

    Solution exo 2

--- src/com/isograd/exercise/IsoContest.java
+++ src/com/isograd/exercise/IsoContest.java
@@ -8,17 +8,34 @@ package com.isograd.exercise;
 import java.util.*;
 
 public class IsoContest {
-    static int D;
+    static int N;
     static int L;
 
+    static HashMap<String,Integer> commands = new HashMap<>();
     public static void main(String[] argv) {
 
         Scanner sc = new Scanner(System.in);
 
-        D = sc.nextInt();
-        L = sc.nextInt();//Duration, ex : 2
+        N = sc.nextInt();
+        sc.nextLine(); //Needed to be able to read the next full line
+        for(int i =0; i< N;i++){
+            String line = sc.nextLine();
+            int val = 1;
+            if(commands.containsKey(line)){
+                 val =  commands.get(line) +1 ;
+            }
+            commands.put(line,val);
+        }
+        commands.forEach((k,v) -> {
+            if(v==2){
+                System.out.println(k);
+            }
+        }  );
 
-        System.out.println(D + L *5);
+
+
+
+
     }
 
 
