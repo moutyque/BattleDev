c41474ac905037f62b6d5749b7e5fc9714ac7efb
Quentin M <azfboom@gmail.com>
2021-06-14 22:28:31 +0200

    Solution exo 4

--- src/com/isograd/exercise/IsoContest.java
+++ src/com/isograd/exercise/IsoContest.java
@@ -9,37 +9,64 @@ import java.util.*;
 
 public class IsoContest {
     static int N;
-    static int L;
+    static String ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    static HashMap<String,Integer> commands = new HashMap<>();
+    static HashMap<String, Integer> commands = new HashMap<>();
+
     public static void main(String[] argv) {
 
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
         sc.nextLine(); //Needed to be able to read the next full line
-        for(int i =0; i< N;i++){
-            String line = sc.nextLine();
-            int val = 1;
-            if(commands.containsKey(line)){
-                 val =  commands.get(line) +1 ;
-            }
-            commands.put(line,val);
+        String line = sc.nextLine();
+
+        String s1 = line.substring(0, N / 2);
+        String s2 = line.substring(N / 2);
+
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+
+        for (int i = 0; i < N / 2; i++) {
+            count1[ALPHA.indexOf(s1.charAt(i))]++;
         }
-        commands.forEach((k,v) -> {
-            if(v==2){
-                System.out.println(k);
+
+        for (int i = 0; i < N / 2; i++) {
+            count2[ALPHA.indexOf(s2.charAt(i))]++;
+        }
+
+        int result = 0;
+        if (sameNbChar(count1, count2)) {
+            result++;
+        }
+
+        for(int i = 0;i<N/2-1;i++){
+            int a1 = ALPHA.indexOf(s1.charAt(i));
+            int a2 = ALPHA.indexOf(s2.charAt(i));
+            count1[a1]--;
+            count2[a1]++;
+
+            count2[a2]--;
+            count1[a2]++;
+            if (sameNbChar(count1, count2)) {
+                result++;
             }
-        }  );
+        }
 
-
-
+        System.out.println(result*2);
 
 
     }
 
-
-
+    public static boolean sameNbChar(int[] count1, int[] count2) {
+        for (int i = 0; i < 26; i++) {
+            if (count1[i] != count2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
 }
